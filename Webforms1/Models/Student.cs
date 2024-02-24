c25a8d71bb7bfdf4a233fdf82019ec2180554d6b
Belginbalakrishanan <belginbalakrishnan@gmail.com>
2024-02-24 20:01:56 +0530

    Add project files.

---
+++ Webforms1/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Webforms1.Models
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Email { get; set; }
+        public string? Course { get; set; }
+
+    }
+}
